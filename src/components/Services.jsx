cd50862745beb1e67a93b0e04b348e92a68b8b52
meissadia <meissadia@gmail.com>
2020-01-05 21:54:14 -0700

    Enable and style Services cards as links

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,26 +1,17 @@
 import React from "react"
-// import { Link } from "gatsby"
-import cloudNative from '../images/cloud-native.png'
-import dataScience from '../images/data-science.png'
-import digitalTransformation from '../images/digital-transformation.png'
-import productStrategy from '../images/product-strategy.png'
-
+import { Link } from "gatsby"
+import cloudNative from "../images/cloud-native.png"
+import dataScience from "../images/data-science.png"
+import digitalTransformation from "../images/digital-transformation.png"
+import productStrategy from "../images/product-strategy.png"
 
 export const Services = () => {
   return (
     <section id="services">
-      <h1 className='section-title'>Services</h1>
+      <h1 className="section-title">Services</h1>
       <div className="cards">
-        <ServiceCard
-          title="Cloud Native"
-          url="#cloud"
-          image={cloudNative}
-        />
-        <ServiceCard
-          title="Data Science"
-          url="#data"
-          image={dataScience}
-        />
+        <ServiceCard title="Cloud Native" url="#cloud" image={cloudNative} />
+        <ServiceCard title="Data Science" url="#data" image={dataScience} />
         <ServiceCard
           title="Digital Transformation"
           url="#transformation"
@@ -38,9 +29,11 @@ export const Services = () => {
 
 const ServiceCard = ({ title, url, image }) => {
   return (
+    <Link to={url}>
       <div className="card">
         <img className="circle" src={image} alt="service icon" />
         <h2 className="title">{title}</h2>
       </div>
+    </Link>
   )
 }
